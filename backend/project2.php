caf1311df886dcfbfe16ab80aac8f4aae2a34e00
Pranav Dhinakar <pranavdhinakar@gmail.com>
2024-04-29 13:31:54 -0400

    Pathing fix

--- backend/project2.php
+++ backend/project2.php
@@ -3,9 +3,9 @@ header('Content-Type: application/json'); // Ensure we're returning JSON
 
 
 $scriptPaths = [
-    'file_search' => '../scripts/search/file_search.sh',
-    'file_filter' => '../scripts//search/file_filter.sh',
-    'output_formatter' => $baseDir . '../scripts//output/output_formatter.sh',
+    'file_search' => '/../scripts/search/file_search.sh',
+    'file_filter' => '/../scripts//search/file_filter.sh',
+    'output_formatter' => $baseDir . '/../scripts//output/output_formatter.sh',
     'interactive_mode' => $baseDir . '../scripts//interactive/interactive_mode.sh',
     'batch_processing' => $baseDir . '../scripts//batch/batch_processing.sh',
     'file_metadata' => $baseDir . '../scripts//utils/file_metadata.sh',
